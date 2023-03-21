e609e4d1bcf0c2cf56146f8e3374184cbb87a7f7
Dwi Aji Cahyono <dwiaji724@gmail.com>
2023-03-21 07:06:04 +0700

    nested custom

---
+++ Nested Custom Element/article-list.js
@@ -0,0 +1,22 @@
+import './article-item.js'
+ 
+class ArticleList extends HTMLElement {
+  set articles(articles) {
+    this._articles = articles;
+    this.render();
+  }
+ 
+  render() {
+    this._articles.forEach(article => {
+      const articleItemElement = document.createElement('article-item');
+      
+      // memanggil fungsi setter article() pada article-item.
+      articleItemElement.article = article;
+      this.appendChild(articleItemElement);
+    });
+  }
+}
+ 
+customElements.define('article-list', ArticleList);
+//  terdapat fungsi setter articles yang berfungsi untuk 
+//menyimpan nilai articles pada properti this._articles. 
